cc3db74190939b12df34f407f67e421ad36c8da9
poledevk <poledevk@gmail.com>
2018-11-13 18:04:29 +0300

    .

---
+++ aa.js
@@ -0,0 +1,17 @@
+function testArray() {
+    let x;
+    let a = [8, 1, 1, 7, 4, 0];
+    let b = [5, 8, 5, 4, 8];
+
+    for (i in a) {
+
+        x = a + b;
+    }
+    return x;
+
+}
+
+let res = testArray(5);
+
+console.log(res);
+
